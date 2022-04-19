a40cadfa79359b9f79e668eff91de68b21b5937f
Anqi Pang <55934700+AnqiPang@users.noreply.github.com>
2022-04-19 14:33:23 -0700

    Update HelloWorld.go

--- src/HelloWorld.go
+++ src/HelloWorld.go
@@ -1,9 +1,12 @@
-#! /usr/bin/env go run
-
+//usr/bin/env go run "$0" "$@"; exit "$?"
 package main
 
-import "fmt"
+import (
+   "fmt"
+   "os"
+)
 
 func main() {
    fmt.Println("Hello, Kevin!")
+   os.Exit(42)
 }
